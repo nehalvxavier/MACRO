84ae341307fa7a9542c644a7ca45266458e8f961
Nehal <nehalvxavier@gmail.com>
2023-06-25 21:32:51 +0530

    Fixed Display Issue

--- keymaps/vial/config.h
+++ keymaps/vial/config.h
@@ -4,12 +4,6 @@
 #pragma once
 
 #define VIAL_KEYBOARD_UID {0x76, 0x94, 0xED, 0x12, 0x52, 0x04, 0x3B, 0xAF}
-
-#define ENCODERS_PAD_A { GP2, GP4 }
-#define ENCODERS_PAD_B { GP3, GP5 }
-#define ENCODER_DIRECTION_FLIP
-#define ENCODER_RESOLUTIONS { 4, 2 }
-#define ENCODER_MAP_KEY_DELAY 10
-
-#define I2C1_SCL_PIN GP27
 #define I2C1_SDA_PIN GP26
+#define I2C1_SCL_PIN GP27
+#define OLED_DISPLAY_128x64
--- keymaps/vial/keymap.c
+++ keymaps/vial/keymap.c
@@ -33,36 +33,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,   _______, _______
     )
 };
-#if defined(ENCODER_MAP_ENABLE)
-const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
-    [_BASE] =  { ENCODER_CCW_CW(RGB_HUD, RGB_HUI),           ENCODER_CCW_CW(RGB_SAD, RGB_SAI)  },
-    [_CUSTOM] =  { ENCODER_CCW_CW(_______, _______),           ENCODER_CCW_CW(_______, _______)  }
-};
-#endif
 
 #ifdef OLED_ENABLE
-bool oled_task_user(void) {
-    // Host Keyboard Layer Status
-    oled_write_P(PSTR("Layer: "), false);
-
-    switch (get_highest_layer(layer_state)) {
-        case _BASE:
-            oled_write_P(PSTR("Default\n"), false);
-            break;
-        case _CUSTOM:
-            oled_write_P(PSTR("FN\n"), false);
-            break;
-        default:
-            // Or use the write_ln shortcut over adding '\n' to the end of your string
-            oled_write_ln_P(PSTR("Undefined"), false);
-    }
-
-    // Host Keyboard LED Status
-    led_t led_state = host_keyboard_led_state();
-    oled_write_P(led_state.num_lock ? PSTR("NUM ") : PSTR("    "), false);
-    oled_write_P(led_state.caps_lock ? PSTR("CAP ") : PSTR("    "), false);
-    oled_write_P(led_state.scroll_lock ? PSTR("SCR ") : PSTR("    "), false);
+// Draw to OLED
+bool oled_task_user() {
+    // Set cursor position
+    oled_set_cursor(0, 1);
+    
+    // Write text to OLED
+    oled_write("Hello World!", false);
     
     return false;
 }
+
 #endif
